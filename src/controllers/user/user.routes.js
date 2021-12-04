81c8987421496c0c5f717150943b8545afc71973
Pham Hong Duc <iamphduc@gmail.com>
2021-12-04 10:37:57 +0700

    feat: add user api

---
+++ src/controllers/user/user.routes.js
@@ -0,0 +1,75 @@
+const express = require("express");
+
+const userService = require("./user.service");
+const authMiddleware = require("../../middlewares/auth.middleware");
+const asyncHandler = require("../../utils/asyncHandler");
+const pick = require("../../utils/pick");
+
+const userRouter = express.Router();
+
+// Top level middlewares
+userRouter.use(authMiddleware);
+
+// [GET] /api/users/:userId
+userRouter.get(
+  "/:userId",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    const user = await userService.getUserById(userId);
+
+    res.status(200).json({ status: "success", user });
+  })
+);
+
+// [GET] /api/users
+userRouter.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const filter = pick(req.body, ["name", "username", "email"]);
+    const options = pick(req.body, ["sortBy", "limit", "page"]);
+
+    const { results, ...rest } = await userService.queryUsers(filter, options);
+
+    res.status(200).json({ status: "success", users: results, ...rest });
+  })
+);
+
+// [POST] /api/users
+userRouter.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const createBody = pick(req.body, ["name", "username", "password", "email", "role"]);
+
+    const { user, token } = await userService.createUser(createBody);
+
+    res.status(200).json({ status: "success", user, token });
+  })
+);
+
+// [PUT] /api/users/:userId
+userRouter.put(
+  "/:userId",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+    const updateBody = pick(req.body, ["name", "username", "email", "role"]);
+
+    const user = await userService.updateUser(userId, updateBody);
+
+    res.status(200).json({ status: "success", user });
+  })
+);
+
+// [DELETE] /api/users/:userId
+userRouter.delete(
+  "/:userId",
+  asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    const user = await userService.deleteUser(userId);
+
+    res.status(200).json({ status: "success", user });
+  })
+);
+
+module.exports = userRouter;
---
+++ src/controllers/user/user.service.js
@@ -0,0 +1,41 @@
+const User = require("../../models/User");
+const ApiError = require("../../utils/ApiError");
+
+const getUserById = async (userId) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return user;
+};
+
+const queryUsers = async (filter, options) => {
+  const users = await User.paginate(filter, options);
+  return users;
+};
+
+const createUser = async (createBody) => {
+  const user = new User(createBody);
+  await user.save();
+  const token = await user.generateAuthToken();
+  return { user, token };
+};
+
+const updateUser = async (userId, updateBody) => {
+  const user = await User.findByIdAndUpdate(userId, updateBody);
+  return user;
+};
+
+const deleteUser = async (userId) => {
+  await User.findByIdAndRemove(userId);
+  return null;
+};
+
+module.exports = {
+  getUserById,
+  queryUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
