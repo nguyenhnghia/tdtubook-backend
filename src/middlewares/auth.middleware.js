23e593c0a7df3677528bfe13ae3108b5b5065d6a
Pham Hong Duc <iamphduc@gmail.com>
2021-12-04 10:38:32 +0700

    fix: handle no token found

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  const bearer = req.header.Authorization.split(" ");
-  const token = bearer[1];
-
-  if (!token) {
-    res.status(401).json({ status: "error", message: "No token found" });
-    return;
-  }
-
   try {
+    const bearer = req.headers.authorization;
+
+    const token = bearer.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ status: "error", message: "No token found" });
+      return;
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.decoded = decoded;
     next();
