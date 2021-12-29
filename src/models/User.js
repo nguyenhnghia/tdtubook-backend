97fe6e7841f9c3b754cb2648e90a58382909054d
Pham Hong Duc <iamphduc@gmail.com>
2021-12-29 14:44:47 +0700

    fix: missing avatar field

--- src/models/User.js
+++ src/models/User.js
@@ -20,6 +20,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/1024px-User-avatar.svg.png",
+    },
     email: {
       type: String,
       required: true,
