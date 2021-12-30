7bf31263862fcafce5cc1beefafd4595d83ad7fc
Pham Hong Duc <iamphduc@gmail.com>
2021-12-30 11:51:17 +0700

    refactor!: make explicit error response

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -8,15 +8,16 @@ const catchError = (err, req, res, next) => {
 
   // Default is Internal Server Error
   let statusCode = 500;
-  let message = "Internal Server Error";
 
   // Check if error is customized
   if (err instanceof ApiError) {
     statusCode = err.statusCode;
-    message = err.message;
   }
 
-  res.status(statusCode).json({ status: "error", message });
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message,
+  });
 };
 
 module.exports = catchError;
