3095389292ba76357c1d85f75dfd98d8f8688c04
Pham Hong Duc <iamphduc@gmail.com>
2022-01-09 00:55:07 +0700

    feat: socket create post

--- src/socket.io.js
+++ src/socket.io.js
@@ -1,12 +1,16 @@
 const { Server } = require("socket.io");
 
 const init = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
 
   io.on("connection", (socket) => {
     // User create a new post
-    socket.on("post:new", (msg) => {
-      socket.broadcast.emit("post:new", msg);
+    socket.on("post:create", (post) => {
+      socket.broadcast.emit("post:create", post);
     });
   });
 };
